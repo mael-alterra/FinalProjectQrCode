4aeecb8fe714455081dee491e59c53fb00b1b24a
Mael <ismail@alterra.id>
2019-12-09 21:11:47 +0700

    fix post absen

--- controllers/AbsenController.go
+++ controllers/AbsenController.go
@@ -3,23 +3,24 @@ package controllers
 import (
 	"FinalProjectQrCode/lib/database"
 	"FinalProjectQrCode/models"
-	"github.com/jinzhu/gorm"
-	"github.com/labstack/echo"
 	"net/http"
 	"strconv"
+
+	"github.com/jinzhu/gorm"
+	"github.com/labstack/echo"
 )
 
-func AbsenController (c echo.Context) error {
+func AbsenController(c echo.Context) error {
 	var time_stamp = (c.Param("time_stamp"))
 	var userid = (c.Param("userid"))
 
-	users, err := database.ReadQrCode(time_stamp)
+	_, err := database.ReadQrCode(time_stamp)
 	if err != nil {
 		return c.JSON(http.StatusOK, map[string]interface{}{
 			"message": "QR Code tidak valid",
 		})
 	}
-	useridint, _ :=strconv.Atoi(userid)
+	useridint, _ := strconv.Atoi(userid)
 	absens := models.Absens{
 		Model:           gorm.Model{},
 		UserID:          useridint,
@@ -29,6 +30,6 @@ func AbsenController (c echo.Context) error {
 
 	return c.JSON(http.StatusOK, map[string]interface{}{
 		"message": "success absensi",
-		"users":   users,
+		"users":   absens,
 	})
 }
